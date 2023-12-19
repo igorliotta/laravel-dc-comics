698981a99d2ecb31e8ee1843d7116b79c9a881ec
igorliotta <liottaigor@gmail.com>
2023-12-19 16:57:30 +0100

    ADD -  store, edit and update crud

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreComicRequest;
 use App\Http\Requests\UpdateComicRequest;
 use App\Models\Comic;
+use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
@@ -18,13 +19,11 @@ class ComicController extends Controller
         return view('comics.index', compact('comics'));
     }
 
-   public function show($id) {
+   public function show($id) {    
 
+    $comic = Comic::find($id);
 
-    $comic = Comic::findOrFail($id);
-    
     return view('comics.show', compact('comic'));
-
    }
 
    public function create() {
@@ -32,8 +31,41 @@ class ComicController extends Controller
     return view('comics.create');
    }
 
-   public function store() {
+   public function store(Request $request) {
 
-    dd('rotta store');
+    $data = $request->all();
+
+    //dd($data);
+    $new_comic = new Comic();
+
+    $new_comic->title = $data['title'];
+    $new_comic->description = $data['description'];
+    $new_comic->thumb = $data['thumb'];
+    $new_comic->price = $data['price'];
+    $new_comic->series = $data['series'];
+    $new_comic->sale_date = $data['sale_date'];
+    $new_comic->type = $data['type'];
+
+    $new_comic->save();
+
+    return redirect()->route('comics.show', $new_comic);
+   }
+
+   public function edit($id) {
+
+    $comic = Comic::find($id);
+
+    return view('comics.edit', compact('comic'));
+   }
+
+   public function update(Request $request, Comic $comic) {
+
+    $data = $request->all();
+
+    // $comic = Comic::find($id);
+    $comic->update($data);
+
+    // dd($data,$comic);
+    return redirect()->route('comics.show', $comic);
    }
 }
